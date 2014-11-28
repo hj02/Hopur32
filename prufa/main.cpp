e149fb273bd58acd3a1f5c5cda5d1c46d27348a9
Halldora Johannsdottir <hj02@ru.is>
2014-11-28 13:04:47 +0000

    Profadi ad breyta

--- prufa/main.cpp
+++ prufa/main.cpp
@@ -1,8 +1,10 @@
-#include <QCoreApplication>
+#include <iostream>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
 
-    return a.exec();
+    int a;
+
+    return 0;
 }
