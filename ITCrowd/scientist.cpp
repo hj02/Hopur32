e4ffb5fa0beb68f668ea283bdf3cffb43cc7ae14
Agnes <agnes14@ru.is>
2014-11-28 15:05:29 +0000

    test

---
+++ ITCrowd/scientist.cpp
@@ -0,0 +1,26 @@
+#include "scientist.h"
+#include <String>
+
+
+class Scientist(){
+
+   public:
+
+                Scientist();
+
+    private:
+                String name;
+                String sex;
+                String bday;
+                String dday;
+
+}
+
+Scientist::Scientist()
+{
+    name="";
+    sex="";
+    bbday="";
+    dday="";
+
+}
---
+++ ITCrowd/scientist.h
@@ -0,0 +1,10 @@
+#ifndef SCIENTIST_H
+#define SCIENTIST_H
+
+class Scientist
+{
+public:
+    Scientist();
+};
+
+#endif // SCIENTIST_H
